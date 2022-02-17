ddb67bb5b9abf195d7d7b4392ce6e7517266e696
charan <fatalbullet028@gmail.com>
2022-02-18 02:37:58 +0530

    redirect

--- index.js
+++ index.js
@@ -2,6 +2,24 @@ const express = require("express");
 const app = express();
 const port = 4000;
 
+app.use(express.urlencoded({extended: true}));
+
+app.get("/login", (req, res) => {
+    res.sendFile("/home/charan/chat_node/login.html");
+})
+
+app.post("/login", (req, res) => {
+    res.redirect("/login/contact");
+})
+
+app.get("/login/contact", (req, res) => {
+    res.sendFile("/home/charan/chat_node/contact.html");
+})
+
+app.post("/login/contact", (req, res) => {
+    res.send(req.body['contact']);
+})
+
 app.listen(port, ()=>{
     console.log(`listening on ${port}`);
 });
