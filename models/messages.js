10b60a904a3ebeddade72b336300a5f10c60a1d2
charan <fatalbullet028@gmail.com>
2022-02-18 20:01:20 +0530

    Connected to mongodb

---
+++ models/messages.js
@@ -0,0 +1,17 @@
+const mongoose = require("mongoose");
+
+const message = new mongoose.Schema({
+    usr : {
+        type: "String"
+    },
+    passwd : {
+        type: "String"
+    },
+    message : {
+        type: "String"
+    }
+});
+
+const messages = mongoose.model("chats", message);
+
+module.exports = messages;
